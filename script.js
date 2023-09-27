c165308caaa25b322667b41661d74fc56591ee59
Mikael Saha <mikael.saha@esedulainen.fi>
2023-09-27 23:37:10 +0300

    Lisättiin viholliset ja niiden toiminnallisuudet

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let spaceshipX = canvas.width / 2 - spaceshipWidth / 2;
 const spaceshipSpeed = 5;
 let points = 0;
 
-const alienWidth = 30;
-const alienHeight = 20;
+const alienWidth = 25;
+const alienHeight = 15;
 const alienRowCount = 4;
 const alienColCount = 5;
 const aliens = [];
@@ -17,17 +17,90 @@ const aliens = [];
 for (let c = 0; c < alienColCount; c++) {
     aliens[c] = [];
     for (let r = 0; r < alienRowCount; r++) {
-        aliens[c][r] = { x: 0, y: 0, alive: true };
+        const x = c * (alienWidth + 10);
+        const y = r * (alienHeight + 10);
+        aliens[c][r] = { 
+            x,
+            y,
+            alive: true,
+            direction: 1,
+            canShoot: false,
+            alienBulletY: y + alienHeight 
+        };
     }
 }
 
-const bulletWidth = 3;
+const bulletWidth = 2;
 const bulletHeight = 10;
 let bulletX = spaceshipX + spaceshipWidth / 2 - bulletWidth / 2;
 let bulletY = canvas.height - spaceshipHeight;
 let bulletSpeed = 10;
 let bulletFired = false;
 
+// Vihollisten piirtäminen
+function drawAliens() {
+    ctx.fillStyle = "green";
+    for (let c = 0; c < alienColCount; c++) {
+        for (let r = 0; r < alienRowCount; r++) {
+            if (aliens[c][r].alive) {
+                const alienX = aliens[c][r].x;
+                const alienY = aliens[c][r].y;
+                ctx.fillRect(alienX, alienY, alienWidth, alienHeight);
+            }
+        }
+    }
+}
+
+// Vihollisten liikuttaminen ja vihollisten ampuminen satunnaisesti
+function moveAliens() {
+    const alienSpeed = 1.5;
+    let changeDirection = false;
+
+    for (let r = 0; r < alienRowCount; r++) {
+        let leftMostX = canvas.width;
+        let rightMostX = 0;
+
+        for (let c = 0; c < alienColCount; c++) {
+            const alien = aliens[c][r];
+            
+            if (alien.alive) {
+                alien.x += alien.direction * alienSpeed;
+                leftMostX = Math.min(leftMostX, alien.x);
+                rightMostX = Math.max(rightMostX, alien.x + alienWidth);
+
+                if (Math.random() < 0.002 && !alien.canShoot) {
+                    alien.canShoot = true;
+                }
+                if (alien.canShoot) {
+                    alien.alienBulletY += bulletSpeed;
+
+                    if (alien.alienBulletY > canvas.height) {
+                        alien.alienBulletY = alien.y + alienHeight;
+                        alien.canShoot = false;
+                    }
+                }
+            }
+        }
+
+        // Kun reunimmainen alien saavuttaa kentän reunan, suunta vaihtuu.
+        if ( leftMostX <= 0 || rightMostX >= canvas.width) {
+            changeDirection = true;
+        }
+    }
+
+    // Viholliset liikkuvat toiseen suuntaan
+    if (changeDirection) {
+        for (let c = 0; c < alienColCount; c++) {
+            for (let r = 0; r < alienRowCount; r++) {
+                const alien = aliens[c][r];
+
+                if (alien.alive) {
+                    alien.direction *= -1;
+                }
+            }
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -36,20 +109,20 @@ function draw() {
     ctx.fillStyle = 'white';
     ctx.fillRect(spaceshipX, canvas.height - spaceshipHeight, spaceshipWidth, spaceshipHeight);
 
-   
+    // Kutsutaan vihollisten piirtämisfunktiota
+    drawAliens();
+
     for (let c = 0; c < alienColCount; c++) {
         for (let r = 0; r < alienRowCount; r++) {
-            if (aliens[c][r].alive) {
-                const alienX = c * (alienWidth + 10);
-                const alienY = r * (alienHeight + 10);
-                aliens[c][r].x = alienX;
-                aliens[c][r].y = alienY;
-                ctx.fillRect(alienX, alienY, alienWidth, alienHeight);
+            const alien = aliens[c][r];
+
+            if (alien.alive && alien.canShoot) {
+                ctx.fillStyle = "blue";
+                ctx.fillRect(alien.x + alienWidth / 2 - bulletWidth / 2, alien.alienBulletY, bulletWidth, bulletHeight);
             }
         }
     }
 
-   
     if (bulletFired) {
         ctx.fillStyle = 'red';
         ctx.fillRect(bulletX, bulletY, bulletWidth, bulletHeight);
@@ -83,6 +156,9 @@ function draw() {
         }
     }
 
+    // Kutsutaan vihollisten liikuttamisfunktiota
+    moveAliens();
+    
     requestAnimationFrame(draw);
 }
 
